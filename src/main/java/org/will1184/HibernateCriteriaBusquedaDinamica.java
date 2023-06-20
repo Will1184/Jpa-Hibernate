4d940ab48ab16b9f6673e45fbe81cd4615aba8a0
Will1184 <brandonwilliammg@gmail.com>
2023-06-20 10:43:53 -0600

    Seventh-Commit

---
+++ src/main/java/org/will1184/HibernateCriteriaBusquedaDinamica.java
@@ -0,0 +1,64 @@
+package org.will1184;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+import org.hibernate.HibernateError;
+import org.hibernate.HibernateException;
+import org.will1184.entity.Cliente;
+import org.will1184.util.JpaUtil;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class HibernateCriteriaBusquedaDinamica {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Filtro para nombre");
+        String nombre = s.nextLine();
+
+        System.out.println("Filtro para apellido");
+        String apellido = s.nextLine();
+
+        System.out.println("Filtro para la forma de pago ");
+        String formaPago = s.nextLine();
+        EntityManager manager= JpaUtil.getEntityManager();
+
+        CriteriaBuilder builder = manager.getCriteriaBuilder();
+
+        CriteriaQuery<Cliente> query = builder.createQuery(Cliente.class);
+        Root<Cliente> from = query.from(Cliente.class);
+
+        try {
+            List<Predicate> condiciones = new ArrayList<>();
+            if (nombre!= null && !nombre.isEmpty()){
+                condiciones.add(builder.equal(from.get("nombre"),nombre));
+            }
+            if (apellido!= null && !apellido.isEmpty()){
+                condiciones.add(builder.equal(from.get("apellido"),apellido));
+            }
+            if (formaPago!= null && !formaPago.equals("")){
+                condiciones.add(builder.equal(from.get("formaPago"),formaPago));
+            }
+            query.select(from).where(builder.and(condiciones.toArray(new Predicate[condiciones.size()])));
+
+            List<Cliente> clientes = manager.createQuery(query).getResultList();
+            clientes.forEach(System.out::println);
+            manager.getTransaction().commit();
+
+        }catch (IllegalArgumentException e){
+            manager.getTransaction().rollback();
+            e.printStackTrace();
+        }finally {
+            manager.close();
+        }
+
+
+
+    }
+}
